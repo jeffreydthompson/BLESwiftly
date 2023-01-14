4aa6bbfcf63590ae2dc836381e8ca55369726457
Jeffrey Thompson <jefreythompson@gmail.com>
2023-01-14 04:42:48 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BLESwiftly
+
+A description of this package.
---
+++ Tests/BLESwiftlyTests/BLESwiftlyTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BLESwiftly
+
+final class BLESwiftlyTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BLESwiftly().text, "Hello, World!")
+    }
+}
